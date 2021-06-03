d1a992e8c96d19017c89da772044e076ac5a4b4c
seaona <mariona@gmx.es>
2021-06-03 22:29:17 +0200

    TEST: added smart contract unit tests

--- eth-contracts/contracts/SolnSquareVerifier.sol
+++ eth-contracts/contracts/SolnSquareVerifier.sol
@@ -1,38 +1,91 @@
 pragma solidity >=0.4.21 <0.6.0;
+import "./ERC721MintableComplete.sol";
 
 // TODO define a contract call to the zokrates generated solidity contract <Verifier> or <renamedVerifier>
-
-
+contract SquareVerifier {
+    function verifyTx(
+        uint[2] memory A,
+        uint[2] memory A_p,
+        uint[2][2] memory B,
+        uint[2] memory B_p,
+        uint[2] memory C,
+        uint[2] memory C_p,
+        uint[2] memory H,
+        uint[2] memory K,
+        uint[2] memory input
+    )
+    public
+    returns
+    (bool r);
+}
 
 // TODO define another contract named SolnSquareVerifier that inherits from your ERC721Mintable class
+contract SolnSquareVerifier is ERC721MintableComplete {
+    SquareVerifier private verifierContract;
+    // TODO define a solutions struct that can hold an index & an address
+    struct Solution {
+        uint256     index;
+        address     addr;
+        bool        minted;
+    }
+
+    // TODO define an array of the above struct
+    uint256 numberOfSolutions = 0;
+
+    // TODO define a mapping to store unique solutions submitted
+    mapping(bytes32 => Solution) uniqueSolutions;
 
 
+    // TODO Create an event to emit when a solution is added
+    event SolutionAdded(uint256 solution_index, address indexed solution_address);
 
-// TODO define a solutions struct that can hold an index & an address
+    constructor(address verifierAddress)
+        ERC721MintableComplete("Udacity Token", "UDA123")
+        public  {
+        verifierContract = SquareVerifier(verifierAddress);
+    }
 
+    // TODO Create a function to add the solutions to the array and emit the event
+    function addSolution(
+        uint[2] memory A,
+        uint[2] memory A_p,
+        uint[2][2] memory B,
+        uint[2] memory B_p,
+        uint[2] memory C,
+        uint[2] memory C_p,
+        uint[2] memory H,
+        uint[2] memory K,
+        uint[2] memory input
+    ) 
+        public 
+    {
+        bytes32 solutionHash = keccak256(abi.encodePacked(input[0], input[1]));
+        require(uniqueSolutions[solutionHash].addr == address(0), "Solution exists already");
+        
+        bool verified = verifierContract.verifyTx(A, A_p, B, B_p, C, C_p, H, K, input);
+        require(verified, "Solution could not be verified");
 
-// TODO define an array of the above struct
-
-
-// TODO define a mapping to store unique solutions submitted
-
-
-
-// TODO Create an event to emit when a solution is added
-
-
-
-// TODO Create a function to add the solutions to the array and emit the event
-
+        uniqueSolutions[solutionHash] = Solution(numberOfSolutions, msg.sender, false);
+        emit SolutionAdded(numberOfSolutions, msg.sender);
+        numberOfSolutions++;
+        
+    }
 
 
 // TODO Create a function to mint new NFT only after the solution has been verified
 //  - make sure the solution is unique (has not been used before)
 //  - make sure you handle metadata as well as tokenSuplly
 
-  
+    function mintNewNFT(uint a, uint b, address to) public {
+        bytes32 solutionHash = keccak256(abi.encodePacked(a, b));
+        require(uniqueSolutions[solutionHash].addr != address(0), "Solution doesn't exist!");
+        require(uniqueSolutions[solutionHash].minted == false, "Token is already minted for this solution!");
+        require(uniqueSolutions[solutionHash].addr == msg.sender, "Only the solution address can use it to mint a token");
+        super.mint(to, uniqueSolutions[solutionHash].index);
+        uniqueSolutions[solutionHash].minted = true;
+    }
 
-
+}
 
 
 
--- eth-contracts/test/TestERC721Mintable.js
+++ eth-contracts/test/TestERC721Mintable.js
@@ -1,32 +1,71 @@
-var ERC721MintableComplete = artifacts.require('ERC721MintableComplete');
+const expect = require('chai').expect;
+const truffleAssert = require('truffle-assertions');
+const ERC721MintableComplete = artifacts.require('ERC721MintableComplete');
 
 contract('TestERC721Mintable', accounts => {
 
     const account_one = accounts[0];
     const account_two = accounts[1];
 
+    // Constructor variables
+    const name = "UdacityToken";
+    const symbol = "UDA123";
+    const baseTokenURI = "https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/";
+    const zeroAddress = '0x0000000000000000000000000000000000000000';
+
+    let currentOwner;
+    let contractInstance;
+
     describe('match erc721 spec', function () {
+        const tokensIds = [1, 2, 3, 4, 5, 6, 7];
+
         beforeEach(async function () { 
-            this.contract = await ERC721MintableComplete.new({from: account_one});
+            contractInstance = await contractDefinition.new(name, symbol, {from: account_one});
 
             // TODO: mint multiple tokens
+            for (let i=0; i<tokensIds.length-1; i++) {
+                await contractInstance.mint(accounts[i+1], tokensIds[tokensIds.length-1], {from: account_one});
+            }
+
         })
 
         it('should return total supply', async function () { 
-            
+            const totalSupply = await contractInstance.totalSupply.call({from: accounts[9]});
+            expect(Number(totalSupply)).to.equal(tokensIds.length);
         })
 
         it('should get token balance', async function () { 
-            
+            const account1Balance = await contractInstance.balanceOf(accounts[1]);
+            expect(Number(acc3Balance)).to.equal(1);
+
+            const account2Balance = await contractInstance.balanceOf(accounts[9]);     
+            expect(Number(acc9Balance)).to.equal(2);
         })
 
         // token uri should be complete i.e: https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/1
         it('should return token uri', async function () { 
-            
+            const token3Uri = await contractInstance.tokenURI(tokensIds[3]); 
+            expect(token3Uri).to.deep.equal(`${baseTokenURI}${tokensIds[3]}`);
+
+            const token6Uri = await contractInstance.tokenURI(tokensIds[2]); 
+            expect(token6Uri).to.deep.equal(`${baseTokenURI}${tokensIds[2]}`);
+
+            const token9Uri = await contractInstance.tokenURI(tokensIds[1]); 
+            expect(token9Uri).to.deep.equal(`${baseTokenURI}${tokensIds[1]}`);
         })
 
         it('should transfer token from one owner to another', async function () { 
-            
+            let tx = await contractInstance.transferFrom(accounts[1], accounts[2], tokensIds[1], {from: accounts[2]});
+            truffleAssert.eventEmitted(tx, 'Transfer', (ev) => {
+                return expect(ev.from).to.deep.equal(accounts[1]) 
+                       && expect(ev.to).to.equal(accounts[8])
+                       && expect(Number(ev.tokenId)).to.equal(tokensIds[2]);
+            });
+
+            expect(await contractInstance.ownerOf(tokensIds[2])).to.equal(accounts[8]);
+            expect(Number(await contractInstance.balanceOf(accounts[1]))).to.equal(2);
+            expect(Number(await contractInstance.balanceOf(accounts[8]))).to.equal(1);
+            expect(await contractInstance.getApproved(tokensIds[2])).to.equal(zeroAddress);
         })
     });
 
@@ -36,11 +75,11 @@ contract('TestERC721Mintable', accounts => {
         })
 
         it('should fail when minting when address is not contract owner', async function () { 
-            
+            await expectToRevert(contractInstance.mint(account_two, 5, {from: account_one}), 'Function caller is not the contract owner');
         })
 
         it('should return contract owner', async function () { 
-            
+            expect(await contractInstance.owner({from: account_two})).to.equal(currentOwner);
         })
 
     });
